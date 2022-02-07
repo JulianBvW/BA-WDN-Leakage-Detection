7e994c9f364b30dfa108ea8ad760f4b281acdffa
JulianBvW <julianhendrik.bvw@gmail.com>
2022-02-07 10:54:07 +0000

    Added simulation function (with leakages)

--- utils/WaterDistributionNetwork.py
+++ utils/WaterDistributionNetwork.py
@@ -1,3 +1,4 @@
+from networkx.generators.random_graphs import random_powerlaw_tree_sequence
 import wntr
 import numpy as np
 import pandas as pd
@@ -12,7 +13,7 @@ class WDN:
 
     Args:
       inp_file (String): The file path of the .inp file.
-      importnat_nodes (iterable of nodes): List of nodes interesting for ML
+      importnat_nodes (iterable of nodes): List of nodes (Strings) interesting for ML
     """
     self.network = wntr.network.WaterNetworkModel(inp_file)
     self.important_nodes = self.network.get_graph().nodes()
@@ -31,3 +32,35 @@ class WDN:
     print(f"->Important: {sorted(self.important_nodes)}")
     print(f"Edges: {self.network.get_graph().edges()}")
     wntr.graphics.plot_network(self.network, title=self.network.name, node_labels=True, link_labels=True)
+
+  def simulate(self, time_in_hours, leakages=[]):
+    """Runs a simulation on the network considering leakages.
+
+    Args:
+      time_in_hours (Int): Duration of the simulation in hours.
+      leakages (iterable of leakages): Leakages consisting of the node id, the affected area and the start and end time in hours.
+    
+    Returns:
+      Pandas Dataframe with the hour as index and pressure values for every node.
+    """
+
+    # Prepare simulation
+    self.network.reset_initial_values()
+    self.network.options.time.duration = time_in_hours*3600
+
+    # Add leakages
+    for n_id, area, time_start_in_hours, time_end_in_hours in leakages:
+      self.network.get_node(n_id).add_leak(self.network, area=area, start_time=time_start_in_hours*3600, end_time=time_end_in_hours*3600)
+
+    # Run simulation
+    sim = wntr.sim.WNTRSimulator(self.network)
+    results = sim.run_sim()
+
+    # Shape results
+    pressures = results.node["pressure"]
+    pressures['hour'] = list(range(pressures.shape[0]))
+    pressures['hour of the day'] = pressures['hour'] % 24
+    pressures['day'] = pressures['hour'] // 24
+    pressures = pressures.set_index('hour')
+
+    return pressures
